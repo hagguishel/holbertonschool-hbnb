e062bbbdac03870264be3667f5055115448457a3
Pulon Julien <pulonjulien@gmail.com>
2025-06-16 13:38:17 +0200

    add attibutes to place and user class

---
+++ part 2/app/models/place.py
@@ -0,0 +1,23 @@
+from app.models.BaseModel import BaseModel
+from app.models.user import User
+from app.models.review import Review
+
+class place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        self.title = title
+        self.description = description
+        self.price = price
+        self.latitude = latitude
+        self.longitude = longitude
+        self.owner = owner
+        self.amenities = []
+        self.reviews = []
+
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
