fe24fc18bb503c2de47c5305fbc92f43c67a2678
Pulon Julien <pulonjulien@gmail.com>
2025-06-17 11:19:22 +0200

    change attributes from user to private

--- part 2/app/models/user.py
+++ part 2/app/models/user.py
@@ -4,11 +4,10 @@ from app.models.BaseModel import BaseModel
 from email_validator import validate_email, EmailNotValidError
 
 class User(BaseModel):
+    used_emails = set()
 
     def __init__(self, first_name, last_name, email, password, is_admin):
         super().__init__()
-    
-        User.used_emails.add(self._email)
 
         self.first_name = first_name
         self.last_name = last_name
@@ -19,31 +18,31 @@ class User(BaseModel):
         if not self.is_admin_user:
             raise PermissionError("Access denied. Admin privileges required.")
 
-        User.used_emails.add(self._email)
+        User.used_emails.add(self.email)
 
     @property
     def first_name(self):
-        return self._first_name
+        return self.__first_name
 
     @first_name.setter
     def first_name(self, value):
         if not value or len(value) > 50:
             raise ValueError("First name: Required, maximum length of 50 characters.")
-        self._first_name = value
+        self.__first_name = value
 
     @property
     def last_name(self):
-        return self._last_name
+        return self.__last_name
 
     @last_name.setter
     def last_name(self, value):
         if not value or len(value) > 50:
             raise ValueError("Last name: Required, maximum length of 50 characters.")
-        self._last_name = value
+        self.__last_name = value
 
-     @property
+    @property
     def email(self):
-        return self._email
+        return self.__email
 
     @email.setter
     def email(self, value):
@@ -51,7 +50,7 @@ class User(BaseModel):
             raise ValueError("Invalid email format.")
         if value in User.used_emails:
             raise ValueError("Email already in use.")
-        self._email = value
+        self.__email = value
 
     def _is_valid_email(self, email):
         try:
@@ -62,32 +61,35 @@ class User(BaseModel):
         
     @property
     def is_admin(self):
-        return self._is_admin
+        return self.__is_admin
 
     @is_admin.setter
     def is_admin(self, value):
-        self._is_admin = bool(value)
+        self.__is_admin = bool(value)
 
 
 
     def is_admin_user(self):
-        return self.is_admin
+        return self.__is_admin
     
     @property
     def password(self):
-        return self.password
+        return self.__password
     
     @password.setter
     def password(self, value):
+        # You should implement password validation and hashing here
+        self.__password = value
 
     
 
 
     def to_dict(self):
-        return{
-        "id": self.id,
-        "first_name": self.first_name,
-        "last_name": self.last_name,
-        "email": self.email,
-        "password": self.password,
-        "is_admin": self.is_admin_user}
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "password": self.password,
+            "is_admin": self.is_admin
+        }
