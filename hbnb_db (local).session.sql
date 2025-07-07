798eaee1689145aefc24d15e1c98a5e515ab8e5f
Pulon Julien <pulonjulien@gmail.com>
2025-07-07 10:31:11 +0200

    add user and place tables on new clean db

---
+++ hbnb_db (local).session.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS hbnb_db;
+CREATE DATABASE hbnb_db;
+USE hbnb_db;
+
+CREATE TABLE IF NOT EXISTS User (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS Place (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    price DECIMAL(10, 2),
+    latitude FLOAT,
+    longitude FLOAT,
+    owner_id CHAR(36),
+    FOREIGN KEY (owner_id) REFERENCES User(id);
+      
+)
