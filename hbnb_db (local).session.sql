4941bb940c3d36571715e1ec5eb6b75edf6f4142
Pulon Julien <pulonjulien@gmail.com>
2025-07-07 10:56:48 +0200

    add all  tables on new clean db

--- hbnb_db (local).session.sql
+++ hbnb_db (local).session.sql
@@ -20,5 +20,29 @@ CREATE TABLE IF NOT EXISTS Place (
     longitude FLOAT,
     owner_id CHAR(36),
     FOREIGN KEY (owner_id) REFERENCES User(id);
-      
-)
+
+);
+
+CREATE TABLE IF NOT EXISTS Review (
+    id CHAR(36) PRIMARY KEY,
+    text TEXT,
+    rating INT (rating BETWEEN 1 AND 5),
+    user_id CHAR(36),
+    place_id CHAR(36),
+    FOREIGN KEY (user_id) REFERENCES Place(id),
+    FOREIGN KEY (place_id) REFERENCES User(id),
+    CONSTRAINT user_review UNIQUE (user_id, place_id),
+);
+
+CREATE TABLE IF NOT EXISTS Amenity (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS Place_Amenity(
+    place_id CHAR(36),
+    amenity_id CHAR(36),
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES Place(id),
+    FOREIGN KEY (amenity_id) REFERENCES Amenity(id),
+);
